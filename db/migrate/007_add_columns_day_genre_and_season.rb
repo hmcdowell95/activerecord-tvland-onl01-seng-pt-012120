f9d0a7080483875dcff21b019def1297735a2460
Harley McDowell <hmcdowell95@gmail.com>
2020-04-06 20:31:53 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_day_genre_and_season.rb
+++ db/migrate/007_add_columns_day_genre_and_season.rb
@@ -2,4 +2,5 @@ class AddColumnsDayGenreAndSeason < ActiveRecord::Migration[5.1]
   
   def change 
     add_column :shows, :day, :string
-    
+    add_column :shows, :genre, :string 
+    add_column :shows, :season, :integer
